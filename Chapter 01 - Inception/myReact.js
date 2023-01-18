06017cdeada98c44db9921ecf93ad1fd9050d8f0
GauravDe-git <gm.gauravde@gmail.com>
2023-01-18 22:38:44 +0530

    Feat: Added Chapter 1 code asg

---
+++ Chapter 01 - Inception/myReact.js
@@ -0,0 +1,5 @@
+const reactHeading = React.createElement("h1",{},"Hello World using React");
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("reactRoot"));
+
+reactRoot.render(reactHeading);
